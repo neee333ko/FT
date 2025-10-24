19334ad65ea99e357696dd181d9c12d4cf7d9c89
neee333ko <1260644864@qq.com>
2025-10-24 10:17:16 +0800

    feat: add train_LIFnode.py

--- cifar10net/train_LIF.py
+++ cifar10net/train_LIF.py
@@ -19,16 +19,21 @@ class Cifar10NetTrainer(train_classify.Trainer_step):
         parser.add_argument('--cupy', action="store_true", help="set the neurons to use cupy backend")
         return parser
 
+    
+    def get_tb_logdir_name(self, args):
+        tb_dir = super().get_tb_logdir_name(args) + '_LIFNode'
+        return tb_dir
+
 
     def load_model(self, args, num_classes):
-        spiking_neurons = [neuron.IFNode] * 8
+        spiking_neurons = [neuron.LIFNode] * 8
     
         if args.model in parametric_lif_net.__all__:
             model = parametric_lif_net.__dict__[args.model](spiking_neurons=spiking_neurons,
                                                         surrogate_function=surrogate.ATan(), detach_reset=True)
             functional.set_step_mode(model, step_mode='s')
             if args.cupy:
-                functional.set_backend(model, 'cupy', neuron.IFNode)
+                functional.set_backend(model, 'cupy', neuron.LIFNode)
 
             return model
         else:
@@ -36,7 +41,7 @@ class Cifar10NetTrainer(train_classify.Trainer_step):
         
         
 if __name__ == "__main__":
-    # nohup python train.py --data-path ~/workspace/dataset --model CIFAR10Net --device cuda:5 -b 50 --epochs 50 -j 4 --lr 1e-3 --time-step 20 --opt adamw > ./logs/train_log.log 2>&1 &
+    # nohup python train_LIF.py --data-path ~/workspace/dataset --model CIFAR10Net --device cuda:4 -b 50 --epochs 50 -j 4 --lr 1e-3 --time-step 20 --opt adamw > ./logs/train_LIF_log.log 2>&1 &
     trainer = Cifar10NetTrainer()
     args = trainer.get_args_parser().parse_args()
     trainer.main(args)
